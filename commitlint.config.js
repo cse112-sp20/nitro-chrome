8e0cc8685e83110e14e0283e6370468c8292f697
DimitriMatevossian <dimitri.matevossian@gmail.com>
2020-05-22 18:31:37 -0700

    ci: added commitlint files

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 120],
+        'subject-case': [0],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'refactor',
+                'test',
+                'perf',
+                'bump',
+                'chore',
+                'revert',
+                'build',
+                'ci',
+                'style'
+            ]
+        ],
+        'scope-empty': [0]
+    }
+};
+

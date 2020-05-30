ea8d2ba318f9e89acf5a6d13ff2f5fee460a9447
liyanxun <liyanxun1128@gmail.com>
2020-05-30 16:57:29 -0700

    style: updated css for user profile

--- extension/JavaScript/userProfile.js
+++ extension/JavaScript/userProfile.js
@@ -1,14 +1,15 @@
 /*==============================================================
 Functionality of BACK button --> redirect to 'Leaderboard' screen
 ==============================================================*/
-let backBtn = document.getElementById('back');
+let backBtn = document.getElementById("back");
 function gotoUsers() {
     location.href = "./users.html";
 }
-backBtn.addEventListener('click', gotoUsers);
+backBtn.addEventListener("click", gotoUsers);
 
-// let table = document.getElementById("user-profile-table");
-
+/*==============================================================
+Display data for all fields:
+==============================================================*/
 let username = document.getElementById("username");
 username.innerHTML = localStorage.getItem("curr_user");
 
--- extension/JavaScript/users.js
+++ extension/JavaScript/users.js
@@ -1,20 +1,21 @@
 const tasks_endpoint = "http://ec2-54-227-1-34.compute-1.amazonaws.com/users";
-// // GET tasks
+
+// GET user data
 let response = null;
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         response = JSON.parse(xhr.responseText);
         console.log(response);
-        //console.log(Object.keys(response));
-        //console.log(Object.keys(response)[0]);
-        //console.log(response[Object.keys(response)[0]]);
         useJSON(response);
     }
 };
 xhr.open("GET", tasks_endpoint, true);
 xhr.send();
 
+/*==============================================================
+Use  JSON data/values to  then populate the table: 
+==============================================================*/
 function useJSON(response) {
     let myMap = new Map();
     let users = Object.keys(response);
@@ -29,25 +30,25 @@ function useJSON(response) {
 /*==============================================================
 Functionality of BACK button --> redirect to 'Leaderboard' screen
 ==============================================================*/
-let backBtn = document.getElementById('back');
+let backBtn = document.getElementById("back");
 function gotoLeaderboard() {
     location.href = "./leaderboard.html";
 }
-backBtn.addEventListener('click', gotoLeaderboard);
+backBtn.addEventListener("click", gotoLeaderboard);
 
 function populateTable(value, key) {
     let table = document.getElementById("user-table");
     let anchorUser = document.createElement("a");
     // Insert a new row at the end of the table
     let newRow = table.insertRow(-1);
-    // create/insert 2 new <td> (table data/cell) elements in the new row
+    // create/insert 1 new <td> (table data/cell) element in the new row
     let cellUser = newRow.insertCell(0); // Team Name  
-    // create/insert the contents of the new cells
+    // create/insert the contents of the new cell
     let cellUserText = document.createTextNode(`${key}`);
 
     anchorUser.appendChild(cellUserText);
     anchorUser.onclick = function () {
-        // Store the team NAME that was clicked for reference for other screens
+        // Store the data that was clicked for reference for other screens
         localStorage.setItem("curr_user", key);
         localStorage.setItem("curr_team", value.team);
         localStorage.setItem("curr_points_completed", value.points_completed);

b151dd5ece9ec535f3a6f5287d940c7e45d6ee88
Austin Cheong <aucheong@ucsd.edu>
2020-06-10 00:51:59 -0700

    test: asc sort of map points

---
+++ extension/JavaScript/sortPointsAsc.test.js
@@ -0,0 +1,17 @@
+/* global require:writable, test:writable, expect:writable */
+const sortPointsAsc = require("./sortPointsAsc");
+
+let unsorted = new Map();
+let answer = new Map();
+unsorted.set("teamA", 9);
+unsorted.set("teamB", 55);
+unsorted.set("teamC", 0);
+
+answer.set("teamC", 0);
+answer.set("teamA", 9);
+answer.set("teamB", 55);
+
+test('sort map of name:points in ascending order of points', () => {
+  expect(sortPointsAsc(unsorted)).toEqual(answer);  // Check if arrays are equal
+//   expect(sortPointsAsc(unsorted)).not.toBe(answer); // Check for pass by value
+});
